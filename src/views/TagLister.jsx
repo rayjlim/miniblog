de429a4a73f7e4f78ab3261ec99ff5ade0165589
Ray <rayjlim1@gmail.com>
2018-06-19 22:58:52 -0700

    clean up js includes and old nav links

--- src/views/TagLister.jsx
+++ src/views/TagLister.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TagLister = props => {
-    console.log(props.source);
+    
     let result = props.source.match(/#[a-z0-9_]+/gi);
     let tags = result !== null ? result.join(', ') : '';
     return (
