02178bba06f8ee9ec7099f4c88f79e48c5c57323
ray <rayjlim1@gmail.com>
2019-02-24 18:02:50 -0800

    fixing travis build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+var gulp = require('gulp'); 
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
+
 gulp.task('deploy', function() {
   var remotePath = '/travis-test';
   var conn = ftp.create({
