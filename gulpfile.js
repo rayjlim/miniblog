d1560229b305ee4ef91fc35c3a8f16b3928f948e
ray <rayjlim1@gmail.com>
2019-02-24 17:49:00 -0800

    gulp send php files

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 gulp.task('deploy', function() {
-  var remotePath = '/public_html/travis-test';
+  let remotePath = '/travis-test';
   var conn = ftp.create({
     host: '50.63.37.1',
     user: args.user,
     password: args.password,
     log: gutil.log
   });
-  gulp.src(['index.html', './**/*.css'])
+  gulp.src(['*.php', './**/*.css'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
 });
