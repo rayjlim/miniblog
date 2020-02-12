b7399b46ac19f41c1d2020a9ffc73642d798bfc2
ray <rayjlim1@gmail.com>
2020-02-12 00:04:05 -0800

    fix default to show text view, linting

--- src/containers/TextEntryContainer.jsx
+++ src/containers/TextEntryContainer.jsx
@@ -1,122 +1,115 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import moment from "moment";
-import EntryList from "../views/EntryList.jsx";
-import EntrySearchBar from "../views/EntrySearchBar.jsx";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import EntryList from '../views/EntryList.jsx';
+import EntrySearchBar from '../views/EntrySearchBar.jsx';
 
-import EntryApi from "../api/EntryApi";
+import EntryApi from '../api/EntryApi';
 
 class TextEntryContainer extends Component {
+	constructor(props) {
+		console.log('TextEntryContainer');
+		super(props);
+		const current = moment();
 
-  constructor(props) {
-    console.log('TextEntryContainer');
-    super(props);
-    const current = moment();
+		this.state = {
+			searchText: ' ',
+			sameDayInput: current.format('YYYY-MM-DD'),
+			month: ''
+		};
+		this.search = this.search.bind(this);
+		this.handleSameDayChange = this.handleSameDayChange.bind(this);
+		this.searchSameDay = this.searchSameDay.bind(this);
+	}
 
-    this.state = {
-      searchText: "",
-      sameDayInput: current.format("YYYY-MM-DD"),
-      month: ""
-    };
-    this.search = this.search.bind(this);
-    this.handleSameDayChange = this.handleSameDayChange.bind(this);
-    this.searchSameDay = this.searchSameDay.bind(this);
-  }
+	componentDidMount() {
+		console.log('TEC: componentDidMount');
+		let loc = window.location + ``;
 
-  componentDidMount() {
-    console.log("TEC: componentDidMount");
-    let loc = window.location + ``;
+		let param = loc.substring(loc.indexOf('?'));
+		console.log(param);
+		let urlParams = new URLSearchParams(param);
 
-    let param = loc.substring(loc.indexOf('?'));
-    console.log(param);
-    let urlParams = new URLSearchParams(param);
+		console.log('urlParams.has: ' + urlParams.has('month'));
+		let month = urlParams.get('month');
+		console.log('passed month: ' + month);
 
-    console.log('urlParams.has: ' + urlParams.has('month'));
-    let month = urlParams.get('month');
-    console.log('passed month: ' + month);
+		if (month !== null) {
+			this.monthCall(month);
+		} else {
+			this.searchCall(this.state.searchText);
+		}
+	}
 
-    if (month !== null) {
-      this.monthCall(month);
-    } else {
-      this.searchCall(this.state.searchText);
-    }
-  }
+	componentDidUpdate() {
+		console.log('TEC: componentDidUpdate');
+		let loc = window.location + '';
 
-  componentDidUpdate() {
-    console.log("TEC: componentWillUpdate");
-    let loc = window.location + ``;
+		let param = loc.substring(loc.indexOf('?'));
+		console.log(param);
+		let urlParams = new URLSearchParams(param);
 
-    let param = loc.substring(loc.indexOf('?'));
-    console.log(param);
-    let urlParams = new URLSearchParams(param);
+		console.log('62.urlParams.has: ' + urlParams.has('month'));
+		let month = urlParams.get('month');
+		console.log('62.passed month: ' + month);
+	}
 
-    console.log('62.urlParams.has: ' + urlParams.has('month'));
-    let month = urlParams.get('month');
-    console.log('62.passed month: ' + month);
+	search(event) {
+		let text = event.target.value;
+		console.log('TEC: search' + text);
+		this.setState({ searchText: text });
+		// debounce the search
+		let timer;
+		if (timer) {
+			clearTimeout(timer);
+		}
+		timer = setTimeout(() => {
+			// console.log("timeout");
 
-    if (this.state.month != month) {
-      this.setState({ month });
-      this.monthCall(month);
-    }
-  }
+			this.searchCall(text);
+		}, 300);
+	}
 
-  search(event) {
+	monthCall(searchParam) {
+		EntryApi.getEntrys('month', searchParam);
+	}
 
-    let text = event.target.value;
-    console.log("TEC: search" + text);
-    this.setState({ searchText: text });
-    // debounce the search
-    let timer;
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      // console.log("timeout");
+	searchCall(searchParam) {
+		EntryApi.getEntrys('searchParam', searchParam);
+	}
 
-      this.searchCall(text);
-    }, 300);
-  }
+	handleSameDayChange(event) {
+		this.setState({ sameDayInput: event.target.value });
+	}
 
-  monthCall(searchParam) {
-    EntryApi.getEntrys("month", searchParam);
-  }
+	searchSameDay() {
+		EntryApi.sameDayEntrys(this.state.sameDayInput);
+	}
 
-  searchCall(searchParam) {
-    EntryApi.getEntrys("searchParam", searchParam);
-  }
+	searchYearMonth(text) {
+		console.log('YM: yearmonth' + text);
+		EntryApi.getEntrys('month', text);
+	}
 
-  handleSameDayChange(event) {
-    this.setState({ sameDayInput: event.target.value });
-  }
-
-  searchSameDay() {
-    EntryApi.sameDayEntrys(this.state.sameDayInput);
-  }
-
-  searchYearMonth(text) {
-    console.log("YM: yearmonth" + text);
-    EntryApi.getEntrys("month", text);
-  }
-
-  render() {
-    console.log("TEC: render.1");
-    return (
-      <div>
-        <EntrySearchBar
-          handleSearch={this.search}
-          searchInput={this.state.searchText}
-          handleSameDay={this.searchSameDay}
-          handleSameDayChange={this.handleSameDayChange}
-          sameDayInput={this.state.sameDayInput}
-        />
-        <EntryList entrys={this.props.entrys} handleDelete={this.deleteEntry} />
-      </div>
-    );
-  }
+	render() {
+		console.log('TEC: render.1');
+		return (
+			<div>
+				<EntrySearchBar
+					handleSearch={this.search}
+					searchInput={this.state.searchText}
+					handleSameDay={this.searchSameDay}
+					handleSameDayChange={this.handleSameDayChange}
+					sameDayInput={this.state.sameDayInput}
+				/>
+				<EntryList entrys={this.props.entrys} handleDelete={this.deleteEntry} />
+			</div>
+		);
+	}
 }
-const mapStateToProps = function (store) {
-  return {
-    entrys: store.postState.posts
-  };
+const mapStateToProps = function(store) {
+	return {
+		entrys: store.postState.posts
+	};
 };
 export default connect(mapStateToProps)(TextEntryContainer);
