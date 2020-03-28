7c7b61cace724f813f87d09d22a76c2405f5f9d4
ray <rayjlim1@gmail.com>
2020-03-27 20:23:12 -0700

    :recycle: initial react markdown in CRA

--- ui-react/src/views/OneDay.js
+++ ui-react/src/views/OneDay.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect, Fragment } from 'react';
 import { NavLink as RouterNavLink } from 'react-router-dom';
 import constants from '../constants';
 import axios from 'axios';
-
-
+import ReactMarkdown from 'react-markdown'; // eslint-disable-line no-unused-vars
+import moment from 'moment';
 
 
 const OneDay = () => {
@@ -42,13 +42,38 @@ const OneDay = () => {
 			<RouterNavLink to="/epcal2/react/">Home</RouterNavLink>
     <button onClick={e => getPreviousDay(e)} >Previous</button>
 			<ul>
-				{data.entries.map((item) => (
-					<li key={item.id}>
-						<a href={item.id}>
-							{item.date}: {item.content}
-						</a>
-					</li>
-				))}
+
+            {data.entries.map((entry) => {
+				let newText = entry.content.replace(/<br \/>/g, '\n');
+				
+                const dateFormated = moment(entry.date).format('ddd MMM, DD YYYY');
+                const calLinkDate = `posts/?gotoYearMonth=${moment(entry.date).format('YYYY-MM')}`;
+                const oneDayLink = `main#/oneDay?date=${moment(entry.date).format('YYYY-MM-DD')}`;
+                let showEntryDate = <a href={oneDayLink}>{dateFormated}</a>;
+                // <a onclick={e=> {location.href=`main#/oneDay?date=${dateFormated}`}}>{dateFormated}</a>);
+                // if (props.editLink) {
+                //     showEntryDate = (
+                //         <a
+                //             onClick={(e) => {
+                //                 e.preventDefault();
+                //                 props.editLink(entry);
+                //             }}
+                //         >
+                //             {moment(entry.date).format('ddd MMM, DD YYYY')}
+                //         </a>
+                //     );
+                //  <TagLister source={newText} /> }
+
+                return (
+                    <li key={entry.id} className="blogEntry">
+                        {showEntryDate}|
+                        <a href={calLinkDate}>Cal</a>|
+                        <ReactMarkdown source={newText} escapeHtml={false} />
+                       
+                    </li>
+                );
+            })}				
+				
 			</ul>
 		</Fragment>
 	);
