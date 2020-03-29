a92cbc6092ed1eb0fd2330436460ba93747d22da
ray <rayjlim1@gmail.com>
2020-03-29 11:34:47 -0700

    :lipstick: new Search view

--- ui-react/src/views/TextEntry.js
+++ ui-react/src/views/TextEntry.js
@@ -4,98 +4,127 @@ import constants from '../constants';
 import axios from 'axios';
 import moment from 'moment';
 import ReactMarkdown from 'react-markdown'; // eslint-disable-line no-unused-vars
+import AddForm from './AddForm.jsx'; //eslint-disable no-unused-vars
+import EditForm from './EditForm.jsx'; //eslint-disable no-unused-vars
+
+const DEBOUNCE_TIME = 300;
 
 const TextEntry = () => {
-		// class ContactForm extends React.Component {
-			const [ data, setData ] = useState({ entries: [] });
-			const [ oDate, setDate ] = useState('');
-		
-			useEffect(() => {
-				console.log('useEffect');
-				console.log('ODB: componentDidMount');
-		
-				let loc = window.location + '';
-		
-				let param = loc.substring(loc.indexOf('?'));
-				console.log('param :', param);
-				let urlParams = new URLSearchParams(param);
-		
-				console.log('urlParams.has date : ' + urlParams.has('date'));
-				const date = urlParams.has('date') ? urlParams.get('date') : moment().format('YYYY-MM-DD');
-				console.log('passed date: ' + date);
-				setDate(date);
-				loadDay(date);
-			}, []);
-		
-			function loadDay(_date) {
-				(async () => {
-					// You can await here
-					const result = await axios(`${constants.REST_ENDPOINT}api/sameDayEntries/?day=${_date}`);
-					console.log('result :', result);
-					setData(result.data);
-					// ...
-				})();
-			}
-		
-			function handleButtonDirection(e) {
-				console.log('event' + e.target);
-		
-				console.log('hbd.' + e.target.value);
-		
-				let date = moment(oDate, 'YYYY-MM-DD');
-				let updateDate = date.add(e.target.value, 'days').format('YYYY-MM-DD');
-				setDate(updateDate);
-				loadDay(updateDate);
-			}
-		
-			return (
-				<Fragment>
-					<h1>OneDay</h1>
-					<RouterNavLink to="/epcal2/react/">Home</RouterNavLink>
-		
-					<button onClick={
-						(e) => handleButtonDirection(e)} className="btn btn-info btn-lrg" value="-1">
-							&lt;&lt;-Prev
-						</button>
-						<input type="text" className="form-control" id="formDpInput" defaultValue={oDate} 
-							onChange={e=>setDate(e.target.value)}/>
-						<button onClick={(e) => handleButtonDirection(e)} className="btn btn-success btn-lrg" value="1">
-							Next-&gt;&gt;
-						</button>
-					{oDate}
-					<ul>
-						{data.entries.map((entry) => {
-							let newText = entry.content.replace(/<br \/>/g, '\n');
-							newText = newText.replace(/..\/uploads/g, `${constants.UPLOAD_PREFIX}uploads`);
-							const dateFormated = moment(entry.date).format('ddd MMM, DD YYYY');
-							const calLinkDate = `posts/?gotoYearMonth=${moment(entry.date).format('YYYY-MM')}`;
-							const oneDayLink = `main#/oneDay?date=${moment(entry.date).format('YYYY-MM-DD')}`;
-							let showEntryDate = <a href={oneDayLink}>{dateFormated}</a>;
-							// <a onclick={e=> {location.href=`main#/oneDay?date=${dateFormated}`}}>{dateFormated}</a>);
-							// if (props.editLink) {
-							//     showEntryDate = (
-							//         <a
-							//             onClick={(e) => {
-							//                 e.preventDefault();
-							//                 props.editLink(entry);
-							//             }}
-							//         >
-							//             {moment(entry.date).format('ddd MMM, DD YYYY')}
-							//         </a>
-							//     );
-							//  <TagLister source={newText} /> }
-		
-							return (
-								<li key={entry.id} className="blogEntry">
-									{showEntryDate}|
-									<a href={calLinkDate}>Cal</a>|
-									<ReactMarkdown source={newText} escapeHtml={false} />
-								</li>
-							);
-						})}
-					</ul>
-				</Fragment>
-			);
+	// class ContactForm extends React.Component {
+	const [ data, setData ] = useState({ entries: [] });
+	const [ searchText, setText ] = useState('');
+	const [ formMode, setFormMode ] = useState(0);
+	const [ entry, setEntry ] = useState({});
+
+	useEffect(() => {
+		console.log('useEffect');
+		getEntries('')
+	}, []);
+
+	function getEntries(text) {
+		console.log('getEntries#text:', text);
+		(async () => {
+			// You can await here
+			const result = await axios(`${constants.REST_ENDPOINT}api/posts/?searchParam=${text}`);
+			console.log('result :', result);
+			setData(result.data);
+			setText(text)
+			// ...
+		})();
+
+	}
+
+	let debouncedSearch = debounce(getEntries, DEBOUNCE_TIME);
+    function search(text) {
+        console.log('TEC: search' + text);        
+		debouncedSearch(text);
+	}
+	
+	const btnShowAddForm = (
+		<button onClick={(e) => showAddForm(e)} className="btn btn-default">
+			Show Add Form
+		</button>
+	);
+
+	function showAddForm(e) {
+		setFormMode(1);
+	}
+	function showEditForm(e, entry) {
+		e.preventDefault();
+		console.log('id :', entry.id);
+		setFormMode(2);
+		setEntry(entry);
+	}
+
+	function resetEntryForm() {
+		setFormMode(0);
+		getEntries(searchText)
+	}
+	function showAddEditForm(mode) {
+		console.log('mode :', mode);
+		if (!mode || mode === 0) {
+			return btnShowAddForm;
+		} else if (mode === 1) {
+			return <AddForm date={''} onSuccess={() => resetEntryForm()} />;
+		} else if (mode === 2) {
+			return <EditForm entry={entry} onSuccess={() => resetEntryForm()} />;
+		}
+	}
+
+	return (
+		<Fragment>
+			<h1>Text Search</h1>
+			<RouterNavLink to="/">Home</RouterNavLink>
+			<RouterNavLink to="/textentry">textentry</RouterNavLink>
+			<RouterNavLink to="/sameday">sameday</RouterNavLink>
+
+
+			<input
+				type="text"
+				className="form-control"
+				defaultValue=''
+				placeholder="Search term"
+				onChange={(e) => search(e.target.value)}
+			/>
+			{searchText}
+			{showAddEditForm(formMode)}
+			<ul>
+			{data.entries.map((entry) => {
+					let newText = entry.content.replace(/<br \/>/g, '\n');
+					newText = newText.replace(/..\/uploads/g, `${constants.UPLOAD_PREFIX}uploads`);
+					const dateFormated = moment(entry.date).format('ddd MMM, DD YYYY');
+					let showEntryDate = (
+						<a onClick={(e) => showEditForm(e, entry)} href="#?">
+							{dateFormated}
+						</a>
+					);
+
+					return (
+						<li key={entry.id} className="blogEntry">
+							{showEntryDate} |
+							<ReactMarkdown source={newText} escapeHtml={false} />
+						</li>
+					);
+				})}
+			</ul>
+		</Fragment>
+	);
 };
 
+
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 export default TextEntry;
