8aafb7461a8717559545f0a82a655ecd696406c5
ray <rayjlim1@gmail.com>
2020-03-29 14:30:09 -0700

    :rocket: update deployment script

--- upload-react.sh
+++ upload-react.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-echo "use upload-react.sh -w to not build webpacknpm "
+echo "use upload-react.sh -b to not build react app "
 . ./env_vars.sh
+
+if ![ -n "$FTP_HOST" ]; then
+    echo "Missing env_vars"
+    exit 2
+fi
+
+while getopts r:s: option
+do
+case "${option}"
+in
+r) RESETSSH=${OPTARG} && echo 'reset ssh';;
+s) BUILD=${OPTARG} && echo 'skip build';;
+esac
+done
 
 cd ui-react
 
-npm run build
+if [ -z $BUILD ]; then
+  npm run build
+fi
 
 # # setup passwordless ssh
-if [ $# -eq 1 ]; then
+if [ ! -z $RESETSSH ]; then
   echo "Reset ssh key"
   ssh-keygen -f "/home/ray/.ssh/known_hosts" -R $FTP_HOST
   ssh-copy-id -f -i ~/.ssh/id_rsa -oHostKeyAlgorithms=+ssh-dss $FTP_USER@$FTP_HOST
 fi
-rsync -rave  'ssh -oHostKeyAlgorithms=+ssh-dss' build/ $FTP_USER@$FTP_HOST:$FTP_TARGETFOLDER 
+
+cd build
+# --dry-run
+rsync -rave  'ssh -oHostKeyAlgorithms=+ssh-dss'  . $FTP_USER@$FTP_HOST:$FTP_TARGETFOLDER 
+ 
+cd static
+rsync -rave 'ssh -oHostKeyAlgorithms=+ssh-dss' --delete  . $FTP_USER@$FTP_HOST:$FTP_TARGETFOLDERstatic/
