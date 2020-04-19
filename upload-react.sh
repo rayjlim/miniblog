949eecb15a3f080dfb92e2c97da4e76b39d67a27
ray <rayjlim1@gmail.com>
2020-04-19 09:00:42 -0700

    :sparkles: clean old react files as part of deploy

--- upload-react.sh
+++ upload-react.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-echo "use upload-react.sh -b to not build react app "
+echo "use upload-react.sh -s 1 to not build react app "
 . ./env_vars.sh
 
-if ![ -n "$FTP_HOST" ]; then
+if [ ! -n "$FTP_HOST" ]; then
     echo "Missing env_vars"
     exit 2
 fi
 
-while getopts r:s: option
+while getopts r:s:c: option
 do
 case "${option}"
 in
 r) RESETSSH=${OPTARG} && echo 'reset ssh';;
 s) BUILD=${OPTARG} && echo 'skip build';;
+c) CLEAN=${OPTARG} && echo 'clean old prod';;
 esac
 done
+
+if [ ! -z $CLEAN ]; then
+  cd ui-react/build/static
+
+  STATIC_DIR=$FTP_TARGETFOLDER"static/"
+  echo $STATIC_DIR
+  pwd
+  rsync -rave 'ssh -oHostKeyAlgorithms=+ssh-dss' --delete  . $FTP_USER@$FTP_HOST:$STATIC_DIR
+
+  exit 0
+fi
 
 cd ui-react
 
@@ -33,9 +45,4 @@ cd build
 # --dry-run
 rsync -rave  'ssh -oHostKeyAlgorithms=+ssh-dss'  . $FTP_USER@$FTP_HOST:$FTP_TARGETFOLDER 
 
-pwd
-cd static
-STATIC_DIR=$FTP_TARGETFOLDER"static/"
-echo $STATIC_DIR
 
-rsync -rave 'ssh -oHostKeyAlgorithms=+ssh-dss' --delete  . $FTP_USER@$FTP_HOST:$STATIC_DIR
