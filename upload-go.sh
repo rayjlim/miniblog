2b8c4bb96a38afe221f438403ce5ac0ce7c60301
ray <rayjlim1@gmail.com>
2020-05-01 09:07:47 -0700

    :rocket: consolidate upload code

--- upload-go.sh
+++ upload-go.sh
@@ -7,17 +7,55 @@ if [ ! -n "$FTP_HOST" ]; then
 fi
 
 echo "Usage: " $0 " [option reset key]"
-while getopts r option
+
+usage() {
+
+  echo ""
+  echo "Usage: "$0" [-s] [-r]"
+  echo ""
+  echo "  -s to Skip Build Step"
+  echo "  -r to reset SSH connection"
+  echo ""
+}
+
+while getopts rs option
 do
-case "${option}"
-in
-r) RESETSSH=${OPTARG} && echo 'reset ssh';;
-esac
+    case "${option}" in
+    r) RESETSSH=true;;
+    s) BUILD=true;;
+    [?])  usage
+        exit 1;;
+    esac
 done
 
 PREP_DIR='../smsblog_prod'
 
-cd $PREP_DIR
+if [ -z "$BUILD" ]; then
+
+  rsync -ravz --exclude-from 'exclude-from-prep.txt' --delete . $PREP_DIR
+  rsync -avz  _rsc/vendor $PREP_DIR/_rsc
+  rsync -avz  "_config/bluehost/SERVER_CONFIG.php"  $PREP_DIR/backend/SERVER_CONFIG.php
+  rsync -avz  _config/.htaccess $PREP_DIR/
+  rsync -avz  _config/cron_script.php $PREP_DIR/
+  rsync -avz  exclude-from-prod.txt $PREP_DIR/
+
+  cd ui-react
+  npm run build
+  cd ..
+
+  rsync -ravz  ui-react/build/ $PREP_DIR/
+
+  pushd .
+  cd $PREP_DIR
+  /usr/local/bin/composer install  --no-dev
+  chmod 755 *.php
+
+  echo "build ready"
+else
+  echo "Skip Build"
+  cd $PREP_DIR
+fi
+
 echo "start upload"
 
 if [ ! -z $RESETSSH ]; then
