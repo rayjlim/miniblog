30d44afce9c1ffcfa9e1fb53b50a9fda5042c456
ray <rayjlim1@gmail.com>
2020-05-03 08:44:10 -0700

    :rotating_light: lint clean up

--- ui-react/src/index.js
+++ ui-react/src/index.js
@@ -2,32 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from "./utils/react-auth0-spa";
-import config from "./auth_config.json";
+import { Auth0Provider } from './utils/react-auth0-spa';
+import config from './auth_config.json';
 import * as serviceWorker from './serviceWorker';
-import history from "./utils/history";
+import history from './utils/history';
 
-const onRedirectCallback = appState => {
-  history.push(
-    appState && appState.targetUrl
-      ? appState.targetUrl
-      : window.location.pathname
-  );
+const onRedirectCallback = (appState) => {
+	history.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname);
 };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
-    redirect_uri= {window.location.origin}
-    audience={config.audience}     // NEW - specify the audience value
-    onRedirectCallback={onRedirectCallback}
-  >
-    <App />
-    </Auth0Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Auth0Provider
+			domain={config.domain}
+			client_id={config.clientId}
+			redirect_uri={window.location.origin}
+			audience={config.audience} // NEW - specify the audience value
+			onRedirectCallback={onRedirectCallback}
+		>
+			<App />
+		</Auth0Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
