20efbff414a36e84d338f439147ece91a5da51de
ray <rayjlim1@gmail.com>
2023-01-16 09:16:29 -0800

    fix: ⬆️ slim4 dependencies  🚧

--- backend/index.php
+++ backend/index.php
@@ -1,58 +1,78 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->setBasePath("/projects/miniblog/backend");
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->post('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world too!");
+    return $response;
+});
+
+$app->run();
+
 // error_reporting(E_ALL & E_STRICT);
 // ini_set('display_errors', '1');
 // ini_set('log_errors', '0');
 // ini_set('error_log', './');
-require 'common_header.php';
+// require 'common_header.php';
 
-$app = new Slim\Slim();
-$app->add(new AuthMiddleware());
+// $app = new Slim\Slim();
+// $app->add(new AuthMiddleware());
 
-$app->get('/security', function ()  use ($app) {
-    echo "{	\"user_id\":\"" . $app->userId . "\"}";
-});
-$app->post('/security', function () use ($app) {
-    echo "{	\"user_id\":\"" . $app->userId . "\"}";
-});
+// $app->get('/security', function ()  use ($app) {
+//     echo "{	\"user_id\":\"" . $app->userId . "\"}";
+// });
+// $app->post('/security', function () use ($app) {
+//     echo "{	\"user_id\":\"" . $app->userId . "\"}";
+// });
 
-$app->post('/', function () use ($app) {
-    $app->redirect('posts/');
-});
+// $app->post('/', function () use ($app) {
+//     $app->redirect('posts/');
+// });
 
-$entryHandler = DAOFactory::EntryHandler($app);
+// $entryHandler = DAOFactory::EntryHandler($app);
 
-$app->get('/api/posts/:id', $entryHandler->itemDetailsApi());
-$app->get('/api/posts/', $entryHandler->listItemsApi());
+// $app->get('/api/posts/:id', $entryHandler->itemDetailsApi());
+// $app->get('/api/posts/', $entryHandler->listItemsApi());
 
-$app->get('/api/sameDayEntries/', $entryHandler->sameDayEntries());
-$app->get('/api/yearMonth', $entryHandler->yearMonthsApi());
+// $app->get('/api/sameDayEntries/', $entryHandler->sameDayEntries());
+// $app->get('/api/yearMonth', $entryHandler->yearMonthsApi());
 
-$cudHandler = DAOFactory::CUDHandler($app);
-$app->post('/api/posts/', $cudHandler->addEntry());
-$app->put('/api/posts/:id', $cudHandler->updateEntry());
-$app->delete('/api/posts/:id', $cudHandler->deleteEntry());
-// handled in AuthMiddleware
+// $cudHandler = DAOFactory::CUDHandler($app);
+// $app->post('/api/posts/', $cudHandler->addEntry());
+// $app->put('/api/posts/:id', $cudHandler->updateEntry());
+// $app->delete('/api/posts/:id', $cudHandler->deleteEntry());
+// // handled in AuthMiddleware
 
-$graphHandler = DAOFactory::GraphHandler($app);
-$app->get('/cron', $graphHandler->logCronCall());
+// $graphHandler = DAOFactory::GraphHandler($app);
+// $app->get('/cron', $graphHandler->logCronCall());
 
-$uploadHandler = DAOFactory::UploadHandler($app);
-$app->post('/uploadImage/', $uploadHandler->upload());
-$app->get('/uploadRotate/', $uploadHandler->rotate());
-$app->get('/uploadResize/', $uploadHandler->resize());
-$app->post('/uploadRename/', $uploadHandler->rename());
+// $uploadHandler = DAOFactory::UploadHandler($app);
+// $app->post('/uploadImage/', $uploadHandler->upload());
+// $app->get('/uploadRotate/', $uploadHandler->rotate());
+// $app->get('/uploadResize/', $uploadHandler->resize());
+// $app->post('/uploadRename/', $uploadHandler->rename());
 
-$app->get('/media/', $uploadHandler->listMedia());
-$app->get('/media/:currentDir', $uploadHandler->listMedia());
-$app->delete('/media/', $uploadHandler->deleteMedia());
+// $app->get('/media/', $uploadHandler->listMedia());
+// $app->get('/media/:currentDir', $uploadHandler->listMedia());
+// $app->delete('/media/', $uploadHandler->deleteMedia());
 
-$logHandler = DAOFactory::LogHandler();
-$app->get('/logs/:logfileName', $logHandler->getUrlHandler());
-$app->get('/logs/', $logHandler->getUrlHandler(''));
-$app->delete('/logs/:logfileName', $logHandler->delete());
+// $logHandler = DAOFactory::LogHandler();
+// $app->get('/logs/:logfileName', $logHandler->getUrlHandler());
+// $app->get('/logs/', $logHandler->getUrlHandler(''));
+// $app->delete('/logs/:logfileName', $logHandler->delete());
 
-$app->get('/ping', function () {
-    echo "{	\"ping\":\"true\"}";
-});
+// $app->get('/ping', function () {
+//     echo "{	\"ping\":\"true\"}";
+// });
 
-$app->run();
+// $app->run();
