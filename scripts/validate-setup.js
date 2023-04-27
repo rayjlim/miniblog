9756caf02cd692525ba8324833d2518898d0f70e
Ray Lim <rayjlim@yahoo.com>
2023-04-27 16:58:00 -0700

    fix: add check for any errors occurance

--- scripts/validate-setup.js
+++ scripts/validate-setup.js
@@ -4,36 +4,43 @@ import requirements from "./requirements.json" assert { type: "json", integrity:
 import fs from "fs";
 console.log("validate project setup");
 
-// console.log("files" + requirements.files.length);
-
+let hasErrors = false;
 requirements.files.forEach((file) => {
   const path = `..${file.name}`;
-  // console.log("check " + path);
+  console.log("check " + path);
   if (fs.existsSync(path)) {
-    console.log(` ${file.name} exists`);
     const data = fs.readFileSync(path,
       {encoding:'utf8', flag:'r'});
 
     if (typeof file.content === "string") {
-      // console.log("check string" + file.content);
 
       if(!data.includes(file.content)){
         console.error(`Missing content ${file.content}`);
+        hasErrors = true;
       }
 
     } else if (Array.isArray(file.content)) {
-      // console.log("check array" + file.content[0]);
       file.content.forEach(content => {
         if(!data.includes(content)){
           console.error(`============Missing content ${content}==========`);
+          hasErrors = true;
         }
       });
     }
   } else {
     console.error(`============Missing file ${path}==========`);
+    hasErrors = true;
   }
 });
 
 console.log("");
+
+if (hasErrors){
+  console.error(`============ See Errors above ==========`);
+} else {
+  console.error(`============ No Error found ==========`);
+
+}
+
 console.log("");
 console.log("");
