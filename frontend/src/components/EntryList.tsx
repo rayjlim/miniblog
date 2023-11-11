61d88072d816423250f3952531638b6d7361d10b
Ray Lim <rayjlim@yahoo.com>
2023-11-11 10:12:43 -0800

    fix: format code🎨

--- frontend/src/components/EntryList.tsx
+++ frontend/src/components/EntryList.tsx
@@ -1,32 +1,38 @@
 import { format, parse } from 'date-fns';
 import MarkdownDisplay from '../components/MarkdownDisplay';
-import {FULL_DATE_FORMAT} from '../constants';
+import { FULL_DATE_FORMAT } from '../constants';
 import '../Types';
 
-const EntryList = ({entries, showEditForm}: { entries: EntryType[], showEditForm: (entry: EntryType)=> void }) => {
-  return       <section className="container">
-  <ul className="entriesList">
-    {entries.map((entry: any) => (
-      <li
-        key={entry.id}
-      >
-        <button
-          onClick={() => showEditForm(entry)}
-          className="plainLink"
-          type="button"
-        >
-          { format(
-            parse(entry.date, FULL_DATE_FORMAT, new Date()),
-            'EEE MM, dd yyyy',
-          )}
-        </button>
-        <div className="markdownDisplay">
-          <MarkdownDisplay source={entry.content} />
-        </div>
-      </li>
-    ))}
-  </ul>
-</section>
+const EntryList = ({
+  entries,
+  showEditForm,
+}: {
+  entries: EntryType[],
+  showEditForm: (entry: EntryType) => void,
+}) => {
+  return (
+    <section className="container">
+      <ul className="entriesList">
+        {entries.map((entry: EntryType) => (
+          <li key={entry.id}>
+            <button
+              onClick={() => showEditForm(entry)}
+              className="plainLink"
+              type="button"
+            >
+              {format(
+                parse(entry.date, FULL_DATE_FORMAT, new Date()),
+                'EEE MM, dd yyyy'
+              )}
+            </button>
+            <div className="markdownDisplay">
+              <MarkdownDisplay source={entry.content} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default EntryList;
