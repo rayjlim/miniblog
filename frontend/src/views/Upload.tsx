42b3af1828183aa80e6e955131671ee6fc0ebe29
Ray Lim <rayjlim@yahoo.com>
2023-11-13 18:52:49 -0800

    fix: upload form to use Header

--- frontend/src/views/Upload.tsx
+++ frontend/src/views/Upload.tsx
@@ -1,20 +1,13 @@
-// import React from 'react';
-import { NavLink as RouterNavLink } from 'react-router-dom';
+import Header from '../components/Header';
 import UploadForm from '../components/UploadForm';
 
 const Upload = () => (
   <>
-    <nav className="navbar navbar-expand-sm navbar-light bg-light">
-      <RouterNavLink to="/oneday">
-        <i className="fa fa-home" />
-        <span>Home</span>
-      </RouterNavLink>
-      <RouterNavLink to="/oneday?pageMode=1">
-        {' '}
-        <i className="fa fa-calendar-check" />
-        <span>Same Day</span>
-      </RouterNavLink>
-    </nav>
+    <Header links={{
+      search: true,
+      oneday: true,
+      sameday: false,
+    }} />
     <UploadForm />
   </>
 );
