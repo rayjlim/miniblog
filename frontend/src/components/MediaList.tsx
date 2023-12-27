e31bb4ae52285767f56f032bba02e6352d7e49a1
Ray Lim <rayjlim@yahoo.com>
2023-12-27 07:24:27 -0800

    change formatting of tags

--- frontend/src/components/MediaList.tsx
+++ frontend/src/components/MediaList.tsx
@@ -3,14 +3,13 @@ import useMediaList from '../hooks/useMediaList';
 
 import './MediaList.css';
 
-
 const MediaList = ({ onMediaSelect }:
   { onMediaSelect: (path: string, file: string) => void }) => {
 
-  const { medias, uploadDirs, currentDir, setCurrentDir, mediaDelete, error, isLoading } = useMediaList();
+  const { medias, uploadDirs, currentDir, setCurrentDir, onDeleteItem, error, isLoading } = useMediaList();
 
   if (isLoading) return <div>Load ...</div>;
-  if (error)  return <div>An error occurred: {(error as RequestError).message}</div>;
+  if (error) return <div>An error occurred: {(error as RequestError).message}</div>;
 
   return (
     <>
@@ -23,14 +22,12 @@ const MediaList = ({ onMediaSelect }:
           ))}
       </div>
       <h2>{`Media (${medias.length})`}</h2>
-      {medias.length && currentDir !== '' && (
-        <ul className="media-preview">
-          {medias.map((key: any) => (
-            <MediaItem key={key} media={key} currentDir={currentDir} selectMedia={onMediaSelect}
-              onChange={mediaDelete} />
-          ))}
-        </ul>
-      )}
+      <ul className="media-preview">
+        {medias.length && currentDir !== '' && medias.map((key: any) => (
+          <MediaItem key={key} media={key} currentDir={currentDir} selectMedia={onMediaSelect}
+            onDelete={onDeleteItem} />
+        ))}
+      </ul>
     </>
   );
 };
