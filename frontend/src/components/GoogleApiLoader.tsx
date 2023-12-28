1d77210a4314ef8a5b5cd1e8ea46ecf451bca44f
Ray Lim <rayjlim@yahoo.com>
2023-12-28 08:25:47 -0800

    fix: only load googleapi key once

--- frontend/src/components/GoogleApiLoader.tsx
+++ frontend/src/components/GoogleApiLoader.tsx
@@ -1,32 +1,37 @@
+import { useRef } from 'react';
 import { useSetting } from './SettingContext';
 import { useEffect } from "react";
 
 const GoogleApiLoader = () => {
   const { GOOGLE_API_KEY } = useSetting() as SettingsType;
+  const isMounted = useRef<boolean>(false);
   function initMap() {
     //required callback for google maps
   }
   useEffect(() => {
-    console.log('load googleapis');
-    // Load the Google Maps API
-    const script = document.createElement('script');
-    if (!GOOGLE_API_KEY) {
-      throw Error('GOOGLE_API_KEY required');
+
+    if (!isMounted) {
+      // Load the Google Maps API
+      const script = document.createElement('script');
+      if (!GOOGLE_API_KEY) {
+        throw Error('GOOGLE_API_KEY required');
+      }
+
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&callback=initMap`;
+      script.defer = true;
+      script.async = true;
+      document.head.appendChild(script);
+      (window as any).initMap = initMap;
+      console.log('api loaded');
+      (isMounted as any).current = true;
+      // Clean up the script tag when the component is unmounted
+      return () => {
+        console.log('remove googleapis');
+        document.head.removeChild(script);
+        delete (window as any).initMap;
+      };
     }
-
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&callback=initMap`;
-    script.defer = true;
-    script.async = true;
-    document.head.appendChild(script);
-    (window as any).initMap = initMap;
-
-    // Clean up the script tag when the component is unmounted
-    return () => {
-      console.log('remove googleapis');
-      document.head.removeChild(script);
-      delete (window as any).initMap;
-    };
-  });
+  }, []);
 
   return (<></>);
 };
