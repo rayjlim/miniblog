2defcf2086b575afb0eceeb2b139cb0f2c33cc28
Ray Lim <rayjlim@yahoo.com>
2024-12-24 15:45:53 -0800

    fix: take out today button to correct spacing

--- frontend/src/components/DateNav.tsx
+++ frontend/src/components/DateNav.tsx
@@ -4,40 +4,43 @@ const DateNav = ({ updateDate, date }: { updateDate: (date: string) => void, dat
   const { inputDate, handleButtonDirection, updateDateInput } = useDateNav(updateDate, date);
 
   return (
-    <div className="grid-3mw container">
-      <button
-        onClick={e => handleButtonDirection(e)}
-        className="btn btn-info btn-lrg"
-        value="-1"
-        id="prevBtn"
-        type="button"
-        title="alt + comma"
-      >
-        <i className="fa fa-chevron-left" />
-        Prev
-      </button>
-      <div>
-        <input
-          type="date"
-          className="form-control"
-          onChange={e => updateDateInput(e)}
-          aria-label="date-input"
-          ref={inputDate}
-        />
+    <>
+      <div className="grid-3mw container col-3">
+        <button
+          onClick={e => handleButtonDirection(e)}
+          className="btn btn-info btn-lrg success"
+          value="-1"
+          id="prevBtn"
+          type="button"
+          title="alt + comma"
+        >
+          <i className="fa fa-chevron-left" />
+          Prev
+        </button>
+        <div>
+          <input
+            type="date"
+            className="form-control"
+            onChange={e => updateDateInput(e)}
+            aria-label="date-input"
+            ref={inputDate}
+          />
 
+        </div>
+
+        <button
+          onClick={e => handleButtonDirection(e)}
+          className="btn btn-success btn-lrg attention"
+          value="1"
+          id="nextBtn"
+          type="button"
+          title="alt + period"
+        >
+          Next
+
+          <i className="fa fa-chevron-right" />
+        </button>
       </div>
-
-      <button
-        onClick={e => handleButtonDirection(e)}
-        className="btn btn-success btn-lrg"
-        value="1"
-        id="nextBtn"
-        type="button"
-        title="alt + period"
-      >
-        Next
-        <i className="fa fa-chevron-right" />
-      </button>
       <button
         onClick={e => handleButtonDirection(e)}
         className="btn btn-warning btn-lrg"
@@ -46,7 +49,8 @@ const DateNav = ({ updateDate, date }: { updateDate: (date: string) => void, dat
       >
         Today
       </button>
-    </div>);
+    </>
+  );
 };
 
 export default DateNav;
