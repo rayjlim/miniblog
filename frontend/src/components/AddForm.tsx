c933689c02e3b85df886b0b3255a4780262ec716
Ray Lim <rayjlim@yahoo.com>
2025-02-25 18:27:51 -0800

    fix: css update

--- frontend/src/components/AddForm.tsx
+++ frontend/src/components/AddForm.tsx
@@ -2,10 +2,28 @@ import MarkdownDisplay from './MarkdownDisplay';
 import useAddForm from '../hooks/useAddForm';
 import LocationForm from './LocationForm';
 
-const AddForm = ({ content, date, onSuccess }: { content: string, date: string, onSuccess: (msg: string, entry: EntryType) => void }) => {
+interface EntryType {
+  id: number;
+  content: string;
+  date: string;
+}
 
-  const { handleAdd, textChange, formContent, dateInput, textareaInput, locationsRef, isLoading } =
-    useAddForm(onSuccess);
+interface AddFormProps {
+  content: string;
+  date: string;
+  onSuccess: (msg: string, entry: EntryType) => void;
+}
+
+const AddForm = ({ content, date, onSuccess }: AddFormProps) => {
+  const {
+    handleAdd,
+    textChange,
+    formContent,
+    dateInput,
+    textareaInput,
+    locationsRef,
+    isLoading
+  } = useAddForm(onSuccess);
 
   return (
     <div className="well">
@@ -14,13 +32,15 @@ const AddForm = ({ content, date, onSuccess }: { content: string, date: string, 
         <div>
           <p className="small">use `@@fa-tag@@` for quick font-awesome icon</p>
           <p className="small">link: [link text](URL)</p>
-          <a className="small" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links" target="_new">
-            Cheatsheet
-          </a>
-          {', '}
-          <a className="small" href="https://fontawesome.com/icons" target="_new">
-            Font Awesome
-          </a>
+          <div className="help-links">
+            <a className="small" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links" target="_blank" rel="noopener noreferrer">
+              Cheatsheet
+            </a>
+            {', '}
+            <a className="small" href="https://fontawesome.com/icons" target="_blank" rel="noopener noreferrer">
+              Font Awesome
+            </a>
+          </div>
         </div>
       </div>
 
@@ -29,45 +49,46 @@ const AddForm = ({ content, date, onSuccess }: { content: string, date: string, 
           id="addFormTextarea"
           ref={textareaInput as any}
           rows={6}
-          onChange={() => textChange()}
+          onChange={textChange}
           className="form-control"
           placeholder="Add ..."
           defaultValue={content}
         />
       </div>
 
-      <div className="editBtns col-3">
+      <div className="editBtns d-flex justify-content-between align-items-center">
         <button
-          onClick={() => handleAdd()}
+          onClick={handleAdd}
           className="btn btn-primary success"
           id="saveBtn"
           type="button"
           disabled={isLoading}
         >
-          <i className="fa fa-save" />
-          Submit
+          <i className="fa fa-save" /> Submit
         </button>
 
-        <div className="form-group">
-          <input type="date"
-            ref={dateInput as any}
-            defaultValue={date} />
-        </div>
-
+        <input
+          type="date"
+          ref={dateInput}
+          defaultValue={date}
+          className="form-control mx-2"
+          style={{ width: '150px'}}
+        />
 
         <button
           type="button"
           onClick={() => onSuccess('', { id: -1, content: '', date: '' })}
-          className="btn btn-warning pull-right attention"
+          className="btn btn-warning attention"
           id="cancelBtn"
         >
-          <i className="fa fa-ban" />
-          Cancel
+          <i className="fa fa-ban" /> Cancel
         </button>
       </div>
+
       <LocationForm ref={locationsRef} />
+
       <div className="markdownDisplay preview dashBorder">
-        <MarkdownDisplay source={formContent as any || ''} />
+        <MarkdownDisplay source={formContent || ''} />
       </div>
     </div>
   );
