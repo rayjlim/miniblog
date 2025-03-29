e2153c6cc0dcea2dc54daa86c7ccdc32b8eab3dc
Ray Lim <rayjlim@yahoo.com>
2025-03-28 19:59:07 -0700

    fix: update vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression()
+  ],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./setupTests.js"
+    setupFiles: "./setupTests.js",
+    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    exclude: ['node_modules', 'dist'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      all: true,
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['node_modules', 'dist'],
+    },
+    alias: {
+      'jest': 'vitest'
+    }
   },
   'process.env': process.env
 })
