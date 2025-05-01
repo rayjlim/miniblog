93af4fdb1998c5b7754db7930e9b19e18f529831
Ray Lim <rayjlim@yahoo.com>
2025-05-01 15:44:59 -0700

    feat: ✨ #44 add preview toggle and styling for AddForm and EditForm
    This commit introduces a preview toggle feature for both AddForm and EditForm components, allowing users to switch the preview layout between horizontal and vertical orientations. Additionally, it adds a new CSS file (AddForm.css) to handle the styling for these components, ensuring a consistent and responsive design across different screen sizes.

--- frontend/src/components/AddForm.tsx
+++ frontend/src/components/AddForm.tsx
@@ -2,6 +2,8 @@ import MarkdownDisplay from './MarkdownDisplay';
 import useAddForm from '../hooks/useAddForm';
 import LocationForm from './LocationForm';
 import { EntryType } from '../Types';
+import { useSetting } from './SettingContext';
+import './AddForm.css';
 
 interface AddFormProps {
   content: string;
@@ -26,85 +28,98 @@ const AddForm = ({ content, date, location, onSuccess }: AddFormProps) => {
     onSuccess('', { id: -1, content: '', date: '' });
   };
 
-  return (
-    <div className="well">
-      <h2>Add Entry</h2>
+  const settings = useSetting();
+  const { PREVIEW } = settings || {};
 
-      <div className="entry-bar mb-3">
-        <div className="help-section">
-          <p className="small mb-1">use `@@fa-tag@@` for quick font-awesome icon</p>
-          <p className="small mb-1">link: [link text](URL)</p>
-          <div className="help-links">
-            <a
-              className="small"
-              href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Cheatsheet
-            </a>
-            {', '}
-            <a
-              className="small"
-              href="https://fontawesome.com/icons"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Font Awesome
-            </a>
+  const changePreview = async () => {
+    if (settings?.updateSetting) {
+      if (PREVIEW === 'down') {
+        await settings.updateSetting('PREVIEW', 'right');
+      } else {
+        await settings.updateSetting('PREVIEW', 'down');
+      }
+    }
+  };
+
+  return (
+    <>
+      <h2>Add Entry</h2>
+      <div className={PREVIEW === 'down' ? 'well-vertical' : 'well'}>
+        <div className="form-container">
+          <form ref={formRef} onSubmit={handleAdd} className="add-form">
+            <div className="form-group mb-1">
+              <textarea
+                id="addFormTextarea"
+                name="content"
+                rows={6}
+                onChange={textChange}
+                className="form-control"
+                placeholder="Add ..."
+                defaultValue={content}
+              />
+            </div>
+
+            <div className="form-actions d-flex justify-content-between align-items-center gap-3 mb-3">
+              <button
+                className="btn btn-primary spaced-link success"
+                id="saveBtn"
+                type="submit"
+                title="alt + s"
+                disabled={isLoading}
+              >
+                <i className="fa fa-save" /> Save
+              </button>
+
+              <input
+                type="date"
+                name="dateInput"
+                defaultValue={date}
+                className="form-control"
+                style={{ width: 'auto' }}
+              />
+
+              <button
+                type="button"
+                onClick={handleCancel}
+                className="btn btn-warning pull-right spaced-link attention"
+                id="cancelBtn"
+              >
+                <i className="fa fa-ban" /> Cancel
+              </button>
+            </div>
+
+            <LocationForm initialLocation={location} />
+          </form>
+          <div className="help-section">
+            <p className="small mb-1">use `@@fa-tag@@` for quick font-awesome icon</p>
+            <p className="small mb-1">link: [link text](URL)</p>
+            <div className="help-links">
+              <a
+                className="small"
+                href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Cheatsheet
+              </a>
+              {', '}
+              <a
+                className="small"
+                href="https://fontawesome.com/icons"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Font Awesome
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-
-      <form ref={formRef} onSubmit={handleAdd} className="add-form">
-        <div className="form-group mb-1">
-          <textarea
-            id="addFormTextarea"
-            name="content"
-            rows={6}
-            onChange={textChange}
-            className="form-control"
-            placeholder="Add ..."
-            defaultValue={content}
-          />
+        <div className="markdown-content preview dashBorder mt-3">
+          <MarkdownDisplay source={markdownContent} />
         </div>
-
-        <div className="form-actions d-flex justify-content-between align-items-center gap-3 mb-3">
-          <button
-            className="btn btn-primary spaced-link success"
-            id="saveBtn"
-            type="submit"
-            title="alt + s"
-            disabled={isLoading}
-          >
-            <i className="fa fa-save" /> Save
-          </button>
-
-          <input
-            type="date"
-            name="dateInput"
-            defaultValue={date}
-            className="form-control"
-            style={{ width: 'auto' }}
-          />
-
-          <button
-            type="button"
-            onClick={handleCancel}
-            className="btn btn-warning pull-right spaced-link attention"
-            id="cancelBtn"
-          >
-            <i className="fa fa-ban" /> Cancel
-          </button>
-        </div>
-
-        <LocationForm initialLocation={location} />
-      </form>
-
-      <div className="markdown-content preview dashBorder mt-3">
-        <MarkdownDisplay source={markdownContent} />
       </div>
-    </div>
+      <button onClick={changePreview}>toggle preview</button>
+    </>
   );
 };
 
--- frontend/src/components/EditForm.tsx
+++ frontend/src/components/EditForm.tsx
@@ -3,6 +3,7 @@ import MarkdownDisplay from './MarkdownDisplay';
 import useEditForm from '../hooks/useEditForm';
 import LocationForm from './LocationForm';
 import { EntryType } from '../Types';
+import { useSetting } from './SettingContext';
 import './EditForm.css';
 
 interface EditFormProps {
@@ -21,36 +22,113 @@ const EditForm = ({ entry, onSuccess }: EditFormProps) => {
     handleDelete,
     isLoading
   } = useEditForm(entry, onSuccess);
+  const settings = useSetting();
+  const { PREVIEW } = settings || {};
+
+  const changePreview = async () => {
+    if (settings?.updateSetting) {
+      if (PREVIEW === 'down') {
+        await settings.updateSetting('PREVIEW', 'right');
+      } else {
+        await settings.updateSetting('PREVIEW', 'down');
+      }
+    }
+  };
 
   return (
-    <div className="well">
+    <>
       <h2>Edit Entry</h2>
 
-      <div className="entry-bar d-flex justify-content-between align-items-start">
-        <div className="help-text">
-          <p className="small">use `@@fa-tag@@` for quick font-awesome icon</p>
-          <p className="small">link: [link text](URL)</p>
-          <div className="links">
-            <a
-              className="small"
-              href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Cheatsheet
-            </a>
-            {', '}
-            <a
-              className="small"
-              href="https://fontawesome.com/icons"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Font Awesome
-            </a>
+      <div className={PREVIEW === 'down' ? 'well-vertical' : 'well'}>
+        <form
+          ref={formRef as RefObject<HTMLFormElement>}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSave();
+          }}
+          className="add-form"
+        >
+          <div className="form-group mb-1">
+            <textarea
+              name="content"
+              onChange={textChange}
+              className="form-control"
+              placeholder="Add ..."
+              rows={8}
+              defaultValue={escapedContent}
+            />
           </div>
-        </div>
 
+          <div className="form-actions d-flex justify-content-between align-items-center gap-3 mb-3">
+            <button
+              className="btn btn-primary spaced-link success"
+              data-testid="saveBtn"
+              id="saveBtn"
+              type="submit"
+              title="alt + s"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <><i className="fa fa-spinner fa-spin" /> Saving...</>
+              ) : (
+                <><i className="fa fa-save" /> Save</>
+              )}
+            </button>
+
+            <input
+              type="date"
+              name="dateInput"
+              defaultValue={entry?.date || ''}
+              className="form-control"
+              style={{ width: 'auto' }}
+            />
+
+            <button
+              type="button"
+              onClick={() => onSuccess('', { id: -1, content: '', date: '' })}
+              data-testid="cancelBtn"
+              className="btn btn-warning pull-right spaced-link attention"
+              id="cancelBtn"
+              title="ESC"
+            >
+              <i className="fa fa-ban" /> Cancel
+            </button>
+          </div>
+
+          <div className="mt-3">
+            <LocationForm content={entry?.locations ? JSON.stringify(entry.locations) : ''} />
+          </div>
+          <div className="help-text">
+            <p className="small">use `@@fa-tag@@` for quick font-awesome icon</p>
+            <p className="small">link: [link text](URL)</p>
+            <div className="links">
+              <a
+                className="small"
+                href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Cheatsheet
+              </a>
+              {', '}
+              <a
+                className="small"
+                href="https://fontawesome.com/icons"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Font Awesome
+              </a>
+            </div>
+          </div>
+        </form>
+
+        <div className="markdown-content preview dashBorder mt-3">
+          <MarkdownDisplay source={markdownContent} />
+        </div>
+      </div>
+      <div className="entry-bar d-flex justify-content-between align-items-start">
+        <button onClick={changePreview}>toggle preview</button>
         <button
           onClick={handleDelete}
           data-testid="deleteBtn"
@@ -60,71 +138,7 @@ const EditForm = ({ entry, onSuccess }: EditFormProps) => {
           <i className="fa fa-trash" /> Delete
         </button>
       </div>
-
-      <form
-        ref={formRef as RefObject<HTMLFormElement>}
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSave();
-        }}
-        className="add-form"
-      >
-        <div className="form-group mb-1">
-          <textarea
-            name="content"
-            onChange={textChange}
-            className="form-control"
-            placeholder="Add ..."
-            rows={8}
-            defaultValue={escapedContent}
-          />
-        </div>
-
-        <div className="form-actions d-flex justify-content-between align-items-center gap-3 mb-3">
-          <button
-            className="btn btn-primary spaced-link success"
-            data-testid="saveBtn"
-            id="saveBtn"
-            type="submit"
-            title="alt + s"
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <><i className="fa fa-spinner fa-spin" /> Saving...</>
-            ) : (
-              <><i className="fa fa-save" /> Save</>
-            )}
-          </button>
-
-          <input
-            type="date"
-            name="dateInput"
-            defaultValue={entry?.date || ''}
-            className="form-control"
-            style={{ width: 'auto' }}
-          />
-
-          <button
-            type="button"
-            onClick={() => onSuccess('', { id: -1, content: '', date: '' })}
-            data-testid="cancelBtn"
-            className="btn btn-warning pull-right spaced-link attention"
-            id="cancelBtn"
-            title="ESC"
-          >
-            <i className="fa fa-ban" /> Cancel
-          </button>
-        </div>
-
-        <div className="mt-3">
-          <LocationForm content={entry?.locations ? JSON.stringify(entry.locations) : ''} />
-        </div>
-      </form>
-
-      <div className="markdown-content preview dashBorder mt-3">
-        <MarkdownDisplay source={markdownContent} />
-      </div>
-    </div>
+    </>
   );
 };
 
